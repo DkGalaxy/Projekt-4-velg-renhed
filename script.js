e609ca4d8980bb10618bf46f2d1119c5f90d9ada
frejayun <frej3460@gmail.com>
2024-04-04 11:09:35 +0200

    Videreudvikling af burgermenu + oprettelse af undersider

--- script.js
+++ script.js
@@ -2,3 +2,16 @@
 document.querySelector('.menu-toggle').addEventListener('click', function() {
     document.querySelector('.menu').classList.toggle('active');
   });
+
+// Dropdown-menu
+document.addEventListener('DOMContentLoaded', function() {
+    const dropdownToggles = document.querySelectorAll('.dropdown-toggle');
+  
+    dropdownToggles.forEach(function(toggle) {
+      toggle.addEventListener('click', function(e) {
+        e.preventDefault();
+        const dropdownMenu = this.nextElementSibling;
+        dropdownMenu.classList.toggle('active');
+      });
+    });
+  });
