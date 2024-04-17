9aac4d8e9a9bd9fff9bdf147d231a5376da69424
Alexander <Modige.bjorn@gmail.com>
2024-04-17 12:05:38 +0200

    opdateret bliv frivilllig

--- javascript/script.js
+++ javascript/script.js
@@ -116,7 +116,37 @@ function handleDonate() {
  
  
  
-
+ let slideIndex = 1;
+ showSlides(slideIndex);
+ 
+ function plusSlides(n) {
+   showSlides(slideIndex += n);
+ }
+ 
+ function currentSlide(n) {
+   showSlides(slideIndex = n);
+ }
+ 
+ function showSlides(n) {
+   let slides = Array.from(document.getElementsByClassName("mySlides")); 
+   let dots = Array.from(document.getElementsByClassName("dot")); 
+   if (n > slides.length) { slideIndex = 1; }
+   if (n < 1) { slideIndex = slides.length; }
+   slides.forEach((slide, index) => {
+     slide.style.display = "none";
+     dots[index].classList.remove("active");
+   });
+   slides[slideIndex - 1].style.display = "block";
+   dots[slideIndex - 1].classList.add("active");
+ }
+ 
+ 
+ let dots = Array.from(document.getElementsByClassName("dot"));
+ dots.forEach((dot, index) => {
+   dot.addEventListener('click', () => {
+     currentSlide(index + 1);
+   });
+ });
   
 
   
