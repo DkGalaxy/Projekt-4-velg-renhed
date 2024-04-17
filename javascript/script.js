0e00fcdc4023e139218a2a92c7462edbe723b362
Alexander <Modige.bjorn@gmail.com>
2024-04-17 11:01:22 +0200

    merged code for blivfrivillig

--- javascript/script.js
+++ javascript/script.js
@@ -120,6 +120,33 @@ function handleDonate() {
   alert("tilykke med din donation på " + amount +" kr vi sætter stor pris på det"); 
  }
  
+// slideshowjava
+ let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = Array.from(document.getElementsByClassName("mySlides")); // Convert to true array
+  let dots = Array.from(document.getElementsByClassName("dot")); // Convert to true array
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].classList.remove("active");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].classList.add("active");
+}
  
  
 
