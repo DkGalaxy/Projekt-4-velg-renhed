a52cf32a80c355381ad4db2b0110a05c24574f2b
valde <fe13valseh@gmail.com>
2024-04-17 10:37:17 +0200

    removed console.log
    console.log removed

--- javascript/script.js
+++ javascript/script.js
@@ -110,13 +110,13 @@ document.addEventListener('DOMContentLoaded', function() {
 function handleDonate() {
   let donationIndput = document.querySelector('#donationamount');
   let donationAmount = donationIndput.value;
-  console.log("Donation amount:", donationAmount);
+ 
   alert('Tillykke med din donation på ' + donationAmount + ' kr. Vi sætter stor pris på det!');
 }
   function donate(amount)
    
   {
-    console.log('donation from grid',amount);
+ 
   alert("tilykke med din donation på " + amount +" kr vi sætter stor pris på det"); 
  }
  
